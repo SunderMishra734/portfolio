041b3da3f064259644edd51afbdd82b03d8d49ac
SunderMishra734 <sundermishra734@gmail.com>
2024-04-28 15:29:37 +0530

    added code for skill section in mobile view

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 $(document).ready(function () {
-    $(".nav-bar-container-cls").hide();
+    $(window).resize(function () {
+        if ($(window).width() > 768) {
+            $(".nav-bar-container-cls").show(); 
+        } else {
+            $(".nav-bar-container-cls").hide(); 
+        }
+    });
+  
     $(".close-menu-cls").click(function () {
         $(".nav-bar-container-cls").css('right', '-100px');
-        $(".nav-bar-container-cls").hide();
+        $(".nav-bar-container-cls").fadeOut(1);
         $(".menu-container-cls").show();
     });
 
     $(".menu-container-cls").click(function () {
-        $(".nav-bar-container-cls").show();
+        $(".nav-bar-container-cls").fadeIn(1);
         $(".nav-bar-container-cls").css('right', '0px');
     });
 });
 
 $(document).click(function (e) {
-    if (!$(e.target).is('.nav-bar-container-cls') && !$(e.target).closest('.menu-container-cls').length) {
-        $('.nav-bar-container-cls').css('right', '-100%'); 
-        $(".nav-bar-container-cls").hide();
+    if ($(window).width() <= 768) {
+        if (!$(e.target).is('.nav-bar-container-cls') && !$(e.target).closest('.menu-container-cls').length) {
+            $('.nav-bar-container-cls').css('right', '-100%');
+            $(".nav-bar-container-cls").fadeOut(1);
+        }
     }
 });

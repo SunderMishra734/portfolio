40e2d2d5a62b73fcca3011f65835dc5c15ebb80c
SunderMishra734 <sundermishra734@gmail.com>
2024-04-28 14:42:14 +0530

    handle scrolling problem

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 $(document).ready(function () {
+    $(".nav-bar-container-cls").hide();
     $(".close-menu-cls").click(function () {
-        $(".nav-bar-container-cls").css('right', '-100%');
+        $(".nav-bar-container-cls").css('right', '-100px');
+        $(".nav-bar-container-cls").hide();
         $(".menu-container-cls").show();
     });
 
     $(".menu-container-cls").click(function () {
+        $(".nav-bar-container-cls").show();
         $(".nav-bar-container-cls").css('right', '0px');
     });
 });
@@ -12,5 +15,6 @@ $(document).ready(function () {
 $(document).click(function (e) {
     if (!$(e.target).is('.nav-bar-container-cls') && !$(e.target).closest('.menu-container-cls').length) {
         $('.nav-bar-container-cls').css('right', '-100%'); 
+        $(".nav-bar-container-cls").hide();
     }
 });

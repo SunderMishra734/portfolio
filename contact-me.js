197399c5dbb9adfa5a8d31b63a06f0214d46ad31
SunderMishra734 <sundermishra734@gmail.com>
2024-05-18 16:07:07 +0530

    resolve some bugs

--- contact-me.js
+++ contact-me.js
@@ -7,5 +7,39 @@ $(document).ready(function () {
         var name = $('#name').val();
         var email = $('#email').val();
         var message = $('#message').val();
+        if (name == "" && email == "" && message == "") {
+            alert("Enter Information");
+        }
+        else {
+            if (name == "" && email != "" && message != "") {
+                alert("Please Enter Name")
+            }
+            else if (email == "" && name != "" && message != "") {
+                alert("Please Enter Email")
+            }
+            else if (!ValidateEmail(email) && name == "" && message == "" && email != "" || !ValidateEmail(email) && name != "" && message != "") {
+                alert("Please enter a valid email address.");
+            }
+            else if (message == "" && name != "" && email != "") {
+                alert("Please give some message.");
+            }
+            else if (name != "" && email == "" && message == "") {
+                alert("Please enter email and message");
+            }
+            else if (name == "" && email != "" && ValidateEmail(email) && message == "") {
+                alert("Please enter name and message");
+            }
+            else if (name != "" && email != "" && ValidateEmail(email) && message != "") {
+                alert("your message send successfully...");
+            }
+        }
     });
+
+    function ValidateEmail(email) {
+        var emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        if (!emailPattern.test(email)) {
+            return false;
+        }
+        return true;
+    }
 });
--- skills.js
+++ skills.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     $('.nav-bar-container-cls ul li:nth-child(3) a').addClass("active");
 
     $(".download-resume-cls").click(function () {
-        var pdfUrl = "/Images/resume.pdf";
+        var pdfUrl = "Images/resume.pdf";
         var link = $("<a>");
         link.attr("href", pdfUrl);
         link.attr("download", "sunder_resume.pdf");
